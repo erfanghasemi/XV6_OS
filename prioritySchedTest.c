ad0a5112bc770bae8d76f8051134b40dca42a24d
9731049 <mmghf74@gmail.com>
2021-02-05 12:57:08 +0330

    some modifications implemented on prioritySchedTest.

--- prioritySchedTest.c
+++ prioritySchedTest.c
@@ -8,9 +8,21 @@ void
 averageCBT(Times *times)
 {
     int result = 0;
+    int counter = 0;
+    int class = 6;
+    int class_result = 0;
 
-    for (int i = 0; i < 30; i++){       
+    for (int i = 0; i < 30; i++){  
+        counter++;     
         result += times[i].runningTime;
+        class_result += times[i].runningTime;
+        if (counter == 5){
+            counter = 0;
+            class_result /= 5;
+            printf(1, "the average CBT for class %d is: %d\n", class, class_result);
+            class_result = 0;
+            class--;
+        }
         printf(1, "the CBT for process %d is: %d\n", i, times[i].runningTime);
     }
 
@@ -23,30 +35,55 @@ void
 averageTurnaroundTime(Times *times)
 {
     int result = 0;
+    int counter = 0;
+    int class = 6;
+    int class_result = 0;
 
-    for (int i = 0; i < 30; i++){
-        result += times[i].runningTime + times[i].sleepingTime + times[i].readyTime;
-        printf(1, "the turnAroundTime for process %d is: %d\n", i, times[i].runningTime + times[i].sleepingTime + times[i].readyTime);
+    for (int i = 0; i < 30; i++){  
+        counter++;     
+        result += (times[i].runningTime + times[i].sleepingTime + times[i].readyTime);
+        class_result += (times[i].runningTime + times[i].sleepingTime + times[i].readyTime);
+        if (counter == 5){
+            counter = 0;
+            class_result /= 5;
+            printf(1, "the average TurnAroundTime for class %d is: %d\n", class, class_result);
+            class_result = 0;
+            class--;
+        }
+        printf(1, "the TurnAroundTime for process %d is: %d\n", i, (times[i].runningTime + times[i].sleepingTime + times[i].readyTime));
     }
-        
+
     result /= 30;
 
-    printf(1, "the average turnArounTime is: %d\n", result);
+    printf(1, "the average TurnAroundTime is: %d\n", result);
 }
 
 void
 averageWaitingTime(Times *times)
 {
     int result = 0;
+    int counter = 0;
+    int class = 6;
+    int class_result = 0;
 
-    for (int i = 0; i < 30; i++){
-        result += times[i].sleepingTime + times[i].readyTime;
-        printf(1, "the waitingTime for process %d is: %d\n", i, times[i].sleepingTime + times[i].readyTime);
+    for (int i = 0; i < 30; i++){  
+        counter++;     
+        result += (times[i].sleepingTime + times[i].readyTime);
+        class_result += (times[i].sleepingTime + times[i].readyTime);
+        if (counter == 5){
+            counter = 0;
+            class_result /= 5;
+            printf(1, "the average waitingTime for class %d is: %d\n", class, class_result);
+            class_result = 0;
+            class--;
+        }
+        printf(1, "the waitingTime for process %d is: %d\n", i, (times[i].sleepingTime + times[i].readyTime));
     }
-        
+
     result /= 30;
 
-    printf(1, "the average WaitingTime is: %d\n", result);
+    printf(1, "the average waitingTime is: %d\n", result);
+    
 }
 
 int find_class(int number);
@@ -84,7 +121,6 @@ int main(void)
     for (k = 0; k < 30; k++){
         wait2(&times[k]);
     }
-        
 
     averageCBT(times);
     averageTurnaroundTime(times);
